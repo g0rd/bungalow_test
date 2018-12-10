22c4a17a2b317e7f66eed640a3cc1cd2fa77daa5
g0rd <rdevashrayee@bioconnect.com>
2018-12-09 22:27:23 -0500

    test showcasing other medium level technique

--- cypress/integration/polls.spec.js
+++ cypress/integration/polls.spec.js
@@ -19,22 +19,35 @@ describe('Poll Tests', function(){
 	cy.get('form').submit()
 	
 	cy.url().should('eq', 'http://test-challenge.bungalow.com/polls/2/results/')
+	
   })
   
-  it('single vote', function(){
-	  
-	cy.visit('http://test-challenge.bungalow.com/polls/1')
+  it('vote count increase by 1', function(){
 	
-	cy.get('[type="radio"]').each(function() {
+	cy.visit('http://test-challenge.bungalow.com/polls/1')
 		
-	console.log($el.value)
-	cy.get($el.value).check()
+	cy.get('[type="radio"]').check('4').should('be.checked')
 	
 	cy.get('form').submit()
 	
 	cy.url().should('eq', 'http://test-challenge.bungalow.com/polls/1/results/')
-		
-	})
+	
+	cy.get('ul>li').contains('Red').invoke('text').then((text => {
+            var prevNum = text.split(' ')[2]
+			
+			cy.get('a').contains('Vote again?').click()
+				
+			cy.get('[type="radio"]').check('4').should('be.checked')
+	
+			cy.get('form').submit()
+	
+			cy.url().should('eq', 'http://test-challenge.bungalow.com/polls/1/results/')
+				
+			cy.get('ul>li').contains('Red').invoke('text').then((text => {
+				var postNum = text.split(' ')[2]
+				expect(Number(prevNum)).to.equal(Number(postNum) - 1)
+			}))
+    }))
   })
  
 })
